d3e85078337339a9b7e96f55c13f11465bc62087
nathan lima dias silva <nathanlds179@gmail.com>
2023-08-19 15:57:51 -0300

    atualizando os links

--- src/pages/SobreMim/index.jsx
+++ src/pages/SobreMim/index.jsx
@@ -7,19 +7,21 @@ export default function SobreMim(){
             <div>
                 <img src={perfil} alt="foto de perfil" />
             </div>
+            <h2>
+                Olá, Eu sou Nathan Desenvolvedor front-end
+            </h2>
+            <h3>Bem-vindo ao meu portifolio</h3>
             <p>
-                Olá, Eu sou Nathan Desenvolvedor front-end Bem-vindo ao meu portifolio
-                <br/>
                 Tenho conhecimento em Html, Css, Javascript, Tailwindcss, Bootstrap e Sass e atualmente estou estudando typescript e React e busco me aprimorar cada vez mais. Sou uma pessoa dedicada e apaixonada por tecnologia e jogos.
             </p>
             <ul className={style.links}>
                 <li>
-                    <a href="#" target="_blank">
+                    <a href="https://www.linkedin.com/in/nathan-lima-dias-silva-8413b024b/" target="_blank" rel="noreferrer">
                         <i className="fi fi-brands-linkedin"></i>
                     </a>
                 </li>
                 <li>
-                    <a href="#" target="_blank">
+                    <a href="https://github.com/nahanlds" target="_blank" rel="noreferrer">
                     <i className="fi fi-brands-github"></i>
                     </a>
                 </li>
